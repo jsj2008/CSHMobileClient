6e0cc247391b7ce4d1e5aaf2c9e26951099c94ce
Sean Chain <seanchain@seanchain.local>
2015-03-09 00:07:56 +0800

    initial update

---
+++ 沉思·航/沉思·航/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  沉思·航
+//
+//  Created by Sean Chain on 3/9/15.
+//  Copyright (c) 2015 Sean Chain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ 沉思·航/沉思·航/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  沉思·航
+//
+//  Created by Sean Chain on 3/9/15.
+//  Copyright (c) 2015 Sean Chain. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
